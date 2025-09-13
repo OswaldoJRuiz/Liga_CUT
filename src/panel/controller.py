ffafc9f7c630dce07a371d495554aaf34adf3077
Diego Silva <diego23montes@outlook.com>
2025-09-13 14:18:24 -0600

    upd: comentamos codigo para entenderlo mejor
    upd: detecto que estamos usando errores de fastapi, pero nosotros tenemos nuestro manejo de errores del microservicio, entonces lo cambiamos por nuestro error fabricado

--- src/panel/controller.py
+++ src/panel/controller.py
@@ -2,6 +2,12 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from database.sessions import get_db
 from auth.dependencies import get_current_user
+from exceptions import ForbiddenError
+
+""" 
+estamos reutilizando una dependencia creada por nosotros en depedencies.py de auth
+misma que se utilizada para el endpoint /token (POST) en auth/controller.py
+"""
 
 router = APIRouter(prefix="/admin", tags=["admin"])
 
@@ -10,8 +16,7 @@ async def require_panel_access(
     db: AsyncSession = Depends(get_db),
 ):
     if not any(p.name == "admin:panel.access" for p in current_user.permissions):
-        from fastapi import HTTPException, status
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Forbidden")
+        raise ForbiddenError("You do not have permission to access the admin panel")  # Nosotros tenemos nuestro manejo de errores y lo importamos de exceptions.py
     return current_user
 
 @router.get("/dashboard", dependencies=[Depends(require_panel_access)])
