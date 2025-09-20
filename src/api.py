b710aaa735ba0277d410656e02d151ddd6f6f258
Diego Silva <diego23montes@outlook.com>
2025-09-19 22:10:40 -0600

    update: inscripcion de las apis, para una mejor organizacion del microservicio debido al crecimiento del mismo

--- src/api.py
+++ src/api.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from auth.controller import router as auth_router
-from panel.controller import router as panel_router
+from permissions.controller import perms_router as permissions_router
+from panel_access.dashboard_controller import router as check_access_admin_app
 
 
 def register_routers(app: FastAPI):
     app.include_router(auth_router)
-    app.include_router(panel_router)
+    app.include_router(permissions_router)
+    app.include_router(check_access_admin_app)
